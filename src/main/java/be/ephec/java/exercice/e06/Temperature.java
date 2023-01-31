8b1dafbc63e1b74ce07e52e90019fe2bbfdd1cf0
MichaelHadw <PSR12475@students.ephec.be>
2023-01-31 20:33:19 +0100

    first commit

--- src/main/java/be/ephec/java/exercice/e06/Temperature.java
+++ src/main/java/be/ephec/java/exercice/e06/Temperature.java
@@ -1,4 +1,32 @@
 package be.ephec.java.exercice.e06;
 
 public class Temperature {
+
+    private double temperature;
+    private TemperatureUnit unit;
+
+    public Temperature (double temperature, TemperatureUnit unit)
+    {
+        this.temperature = temperature;
+        this.unit = unit;
+    }
+    public double getValue(TemperatureUnit unit)
+    {
+        if (this.unit == unit)
+        {
+            return temperature;
+        }
+        else if (unit == TemperatureUnit.CELSIUS && this.unit == TemperatureUnit.FAHRENHEIT ) {
+            return (temperature -32) *5./9;
+        }
+        else
+        {
+            return temperature * 9 /5 +32;
+        }
+    }
+    public void setValue (double value, TemperatureUnit unit)
+    {
+        this.temperature = value;
+        this.unit = unit;
+    }
 }
---
+++ src/main/java/be/ephec/java/exercice/e06/TemperatureUnit.java
@@ -0,0 +1,5 @@
+package be.ephec.java.exercice.e06;
+
+public enum TemperatureUnit {
+    CELSIUS, FAHRENHEIT
+}
